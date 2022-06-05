821b5d790ae0e1d845fe1e1c1b1c27604c2545cd
Jonab <jonabsf@gmail.com>
2022-06-05 03:33:50 -0300

    Criação da rotina de validação

--- cli.js
+++ cli.js
@@ -1,11 +1,16 @@
 import chalk from 'chalk';
 import getFile from './index.js';
+import urlValidator from './http-validator.js';
 
 const path = process.argv;
 
 async function textProcess(pathFile) {
   const result = await getFile(pathFile[2]);
-  console.log(chalk.yellow('lista de links'), result);
+  if (path[3] === 'validar') {
+    console.log(chalk.yellow('links validados'), await urlValidator(resulte));
+  } else {
+    console.log(chalk.yellow('lista de links'), result);
+  }
 }
 
 textProcess(path);
---
+++ http-validator.js
@@ -0,0 +1,41 @@
+import fetch from 'node-fetch';
+
+function errorManager(error)
+{
+    throw new Error(error.message);
+}
+
+async function statusCheck(arrayURL)
+{
+    try {
+        const arrayStatus = await Promise
+          .all(arrayURLs
+            .map(async url => {
+              const res = await fetch(url)
+              return res.status;
+        }))
+        return arrayStatus;
+      } catch(erro) {
+        errorManager(erro);
+      } 
+}
+
+
+function arrayURLGenerator(arrayLinks)
+{
+    return arrayLinks.map(linkObject => Object.values(linkObject).join());
+}
+
+async function urlValidator(arrayLinks)
+{
+    const links = arrayURLGenerator(arrayLinks);
+    const statusLink = await statusCheck(links);
+    const results = arrayLinks.map((object, index) => ({
+        ...object,
+        status: statusLinks[index]
+      }))
+      return results;
+}
+
+export default urlValidator;
+
